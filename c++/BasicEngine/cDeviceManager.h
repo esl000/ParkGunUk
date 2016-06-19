52f2d38790e120edddd4b81719911bb70a6f1441
Park <esl000@naver.com>
2016-06-19 19:42:03 +0900

    Edit Device Code

--- c++/BasicEngine/cDeviceManager.h
+++ c++/BasicEngine/cDeviceManager.h
@@ -1,15 +1,34 @@
 #pragma once
 
+#define g_pD3DDevice cDeviceManager::GetInstance()->GetDevice()
+#define g_pD3DContext cDeviceManager::GetInstance()->GetContext()
+
 class cDeviceManager
 {
 private:
 
+	bool m_vsync_enabled;
+	int m_videoCardMemory;
+	char m_videoCardDescription[128];
+	IDXGISwapChain* m_swapChain;
+	ID3D11Device* m_device;
+	ID3D11DeviceContext* m_deviceContext;
+	ID3D11RenderTargetView* m_renderTargetView;
+	ID3D11Texture2D* m_depthStencilBuffer;
+	ID3D11DepthStencilState* m_depthStencilState;
+	ID3D11DepthStencilView* m_depthStencilView;
+	ID3D11RasterizerState* m_rasterState;
+	D3DXMATRIX m_projectionMatrix;
+	D3DXMATRIX m_worldMatrix;
+	D3DXMATRIX m_orthoMatrix;
 
 private:
 	SINGLETONE(cDeviceManager)
 
 public:
 	bool CreateDevice();
-
+	ID3D11Device* GetDevice();
+	ID3D11DeviceContext* GetContext();
+	void Destroy();
 };
 
--- c++/BasicEngine/cMain.cpp
+++ c++/BasicEngine/cMain.cpp
@@ -9,6 +9,7 @@ cMain::cMain()
 
 cMain::~cMain()
 {
+	cDeviceManager::GetInstance()->Destroy();
 }
 
 void cMain::Setup()
@@ -23,5 +24,10 @@ void cMain::Update()
 
 void cMain::Render()
 {
+	//// 백버퍼의 내용을 지웁니다.
+	//g_pD3DContext->ClearRenderTargetView(m_renderTargetView, color);
+	//// 깊이 버퍼를 지웁니다.
+	//g_pD3DContext->ClearDepthStencilView(m_depthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
 
+	//g_pD3DContext->->Present(1, 0);
 }
